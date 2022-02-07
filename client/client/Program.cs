5d918e907764d3fecdac0b08fcead95986bd0a85
mohamedalisc <mohamed01559008276@gmail.com>
2022-02-07 17:57:31 +0200

    first commit

---
+++ client/client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IPAddress h = IPAddress.Parse("127.0.0.1");
+            IPEndPoint hostEndpoint = new IPEndPoint(h, 8000);
+            Socket clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            clientSock.Connect(hostEndpoint);
+
+            byte[] recivedata = new byte[1024];
+            int recivedbytelen = clientSock.Receive(recivedata);
+            string filedata = "";
+            filedata = Encoding.ASCII.GetString(recivedata, 0, recivedbytelen);
+            
+            Console.WriteLine("Server Said: {0}", filedata);
+            Console.WriteLine("Conversation Ended");
+            clientSock.Shutdown(SocketShutdown.Both);
+            clientSock.Close();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ server/server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name : Mohamed Ali Salama hassan\n");
+            Console.WriteLine("ID : 20191700563\n");
+            Console.WriteLine("Department : SC\n");
+            Console.WriteLine("Section : 4\n");
+            IPEndPoint ipaddres = new IPEndPoint(IPAddress.Any, 8000);
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Bind(ipaddres);
+            s.Listen(100);
+            Socket clientSock = s.Accept();
+
+            
+                string filename = args[0];
+                byte[] fileNameByte = Encoding.ASCII.GetBytes(filename);
+                byte[] fileLength = BitConverter.GetBytes(filename.Length);
+                byte[] fileData = File.ReadAllBytes(filename);
+                byte[] clientData = new byte[4 + fileNameByte.Length + fileData.Length];
+                fileLength.CopyTo(clientData, 3);
+                fileNameByte.CopyTo(clientData, 4);
+                fileData.CopyTo(clientData, 4 + fileNameByte.Length);
+                
+                  
+                  clientSock.Send(clientData);
+
+                   clientSock.Close();
+            
+          
+
+
+
+        }
+    }
+}
